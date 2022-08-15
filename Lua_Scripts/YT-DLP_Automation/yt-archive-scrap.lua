0e802a9fc4c3edd8928870bfbf3c6f27c1b1aab4
Kira <83386462+kiram-16@users.noreply.github.com>
2022-08-14 17:58:03 -0700

    Add files via upload

---
+++ Lua_Scripts/YT-DLP_Automation/yt-archive-scrap.lua
@@ -0,0 +1,13 @@
+-- Program Start
+
+-- yt-dlp command iterator
+for line in io.lines ("yta-scrap-urls.txt") do
+    print(line)
+    os.execute(line)
+end
+--Post Script Cleanup
+print("Scrape Completed")
+
+collectgarbage("collect")
+
+-- Program End
