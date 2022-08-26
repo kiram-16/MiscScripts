a9ca26b867853ff3d04a01caec1d56ecccf9fca3
Kira <83386462+kiram-16@users.noreply.github.com>
2022-08-26 02:32:33 -0700

    Add files via upload

---
+++ Lua_Scripts/Unison_Automation/unison_automate.lua
@@ -0,0 +1,16 @@
+-- Program Start
+
+profile_list = {}
+
+--[[
+Removable Drive Mounting
+--]]
+
+
+for i in pairs(profile_list) do
+    print(profile_list[i])
+    unicom =
+    os.execute("unison "..profile_list[i])
+end
+
+-- Program End
